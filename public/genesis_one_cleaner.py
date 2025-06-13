097e3c41dd636ab08865e7078ea3bc035ebda3e4
David Fisher <fisherds@rose-hulman.edu>
2025-06-12 23:14:55 -0400

    progress

--- public/genesis_one_cleaner.py
+++ public/genesis_one_cleaner.py
@@ -1,10 +1,10 @@
 import re
 
-input_path = "genesis_one.json"
-output_path = "genesis_one_edit.json"
+input_path = "public/genesis_one.json"
+output_path = "public/genesis_one_edit.json"
 
 with open(input_path, "r", encoding="utf-8") as infile, open(output_path, "w", encoding="utf-8") as outfile:
     for line in infile:
         # Remove 'Origin:' up to the last '",'
-        new_line = re.sub(r'Origin:.*?",', '', line)
+        new_line = re.sub(r'Origin:.*?",', '",', line)
         outfile.write(new_line)
